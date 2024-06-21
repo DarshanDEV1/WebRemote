ecc70ff52e49b87e6ef912b5d3727e2535ab08fc
Bedaprakash Saidarshan Sahoo <86543754+DarshanDEV1@users.noreply.github.com>
2024-06-21 11:17:50 +0530

    Update Script.js

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-var touchstartX = 0;
-var touchstartY = 0;
-var touchendX = 0;
-var touchendY = 0;
+document.addEventListener('DOMContentLoaded', function()
+{  
+    var touchstartX = 0;
+    var touchstartY = 0;
+    var touchendX = 0;
+    var touchendY = 0;
 
-document.addEventListener('touchstart', function(event) {
-    touchstartX = event.touches[0].pageX;
-    touchstartY = event.touches[0].pageY;
-}, false);
+    document.addEventListener('touchstart', function(event) {
+        touchstartX = event.touches[0].pageX;
+        touchstartY = event.touches[0].pageY;
+    }, false);
 
-document.addEventListener('touchmove', function(event) {
-    event.preventDefault();
-}, false);
+    document.addEventListener('touchmove', function(event) {
+        event.preventDefault();
+    }, false);
 
-document.addEventListener('touchend', function(event) {
-    touchendX = event.changedTouches[0].pageX;
-    touchendY = event.changedTouches[0].pageY;
-    handleSwipe();
-}, false);
+    document.addEventListener('touchend', function(event) {
+        touchendX = event.changedTouches[0].pageX;
+        touchendY = event.changedTouches[0].pageY;
+        handleSwipe();
+    }, false);
 
-function handleSwipe() {
-    if (touchendX < touchstartX) {
-        // Swipe left
-        console.log('Swiped left');
-        document.getElementById("Debug").innerHTML = "Swiped Left";
-    } else if (touchendX > touchstartX) {
-        // Swipe right
-        console.log('Swiped right');
-        document.getElementById("Debug").innerHTML = "Swiped Right";
+    function handleSwipe() {
+        if (touchendX < touchstartX) {
+            // Swipe left
+            console.log('Swiped left');
+            document.getElementById("Debug").innerHTML = "Swiped Left";
+        } else if (touchendX > touchstartX) {
+            // Swipe right
+            console.log('Swiped right');
+            document.getElementById("Debug").innerHTML = "Swiped Right";
+        }
     }
-}
+});
